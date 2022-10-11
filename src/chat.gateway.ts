02da201e00f785b2198aaac0c146f5a7a4151f25
Jenish MS <jenishmstephen@gmail.com>
2022-10-12 00:02:45 +0530

    initial commit

---
+++ src/chat.gateway.ts
@@ -0,0 +1,28 @@
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Socket } from 'dgram';
+
+@WebSocketGateway(81, {
+  cors: {
+    origin: '*',
+  },
+})
+export class ChatGateway {
+  @WebSocketServer()
+  server;
+
+  @SubscribeMessage('message')
+  handleMessage(
+    @MessageBody() message: string,
+    @ConnectedSocket() client: Socket,
+  ): void {
+    console.log(message);
+    client.emit('message', 'Message Received');
+    this.server.emit('message', message);
+  }
+}
